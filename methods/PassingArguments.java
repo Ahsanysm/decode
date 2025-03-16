0a18e9bd8d91da3400131f40ba3511817ea8187d
Ahsanysm <ahsaahmadbhat145@gmail.com>
2025-03-16 11:33:51 +0530

    passing an argument

---
+++ methods/PassingArguments.java
@@ -0,0 +1,20 @@
+package methods;
+
+public class PassingArguments {
+//    public static void greet(String name) {
+//        System.out.println("Good morning " +name);
+//    }
+//    public static void main(String[] args) {
+//        greet("janab");//this is passing argument or we are passing here a string to psv greet
+//        greet("ahsan");
+//
+//    }
+public static void main(String[] args) {
+    sum(75,12);
+}
+public static void sum(int a, int b){
+    System.out.println(a+b);
+}
+}
+
+
