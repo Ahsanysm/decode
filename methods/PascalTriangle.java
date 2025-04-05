9390911febf064164d0c24a92776e4249a7004ac
Ahsanysm <ahsaahmadbhat145@gmail.com>
2025-04-05 10:48:22 +0530

    Pascacl triangle

---
+++ methods/PascalTriangle.java
@@ -0,0 +1,33 @@
+package methods;
+//print pascal triangle taking input from user
+/*      1
+       1 1
+      1 2 1
+     1 3 3 1
+    1 4 6 4 1
+ */
+import java.util.Scanner;
+public class PascalTriangle {
+    public static int fact(int x){//this is function of factorial
+        int xFact=1;
+        for(int i=1;i<=x;i++){
+            xFact *= i;
+        }
+        return xFact;
+    }
+    public static void main(String[] args) {
+         Scanner sc= new Scanner(System.in);
+        System.out.println("enter value of n");
+         int n=sc.nextInt();
+        for (int i = 0; i <=n ; i++) {
+            for (int j = 1; j <=n-i ; j++) {//spaces
+                System.out.print(" "+" ");
+            }
+            for (int j = 0; j <=i ; j++) {
+                int icj=fact(i)/(fact(j)*fact(i-j));
+                System.out.print(icj+"   ");
+            }
+            System.out.println();
+        }
+    }
+}
